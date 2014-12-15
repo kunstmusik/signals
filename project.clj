18a9e13291de2ee3d05066e40166a0c8cb195113
Steven Yi <stevenyi@gmail.com>
2014-12-15 13:34:38 -0500

    added :scm tag

--- project.clj
+++ project.clj
@@ -5,4 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-alpha4"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 ])
+                 ]
+  :scm {:name "git"
+        :url "https://github.com/kunstmusik/signals.git" 
+        })
